da7f42bd01e42d84cb383bad853a76e6edeb8008
stastuz98 <mr.stastuz98@yandex.ru>
2019-11-26 18:50:12 +0400

    INSERT csv_file

---
+++ csv_file.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import csv
+import json
+
+#csvTOjson
+
+FILE_NAME1 = 'TOjson.json' 
+FILE_NAME2 = 'csv_file.csv'
+
+dictionary = []
+
+def read_csv_file(file_name):
+    with open(file_name) as csvFile:
+        csvReader = csv.DictReader(csvFile)
+        for row in csvReader:
+            dictionary.append(row)
+    return dictionary
+
+def toJson(data):
+    with open(FILE_NAME1, 'w') as jsonFile:  
+        jsonFile.write(json.dumps(data,indent=4))
+
+
+toJson(read_csv_file('dict.csv'))
--- json_file.py
+++ json_file.py
@@ -4,18 +4,13 @@ import csv
 FILE_NAME1 = 'csvTOjson.json' 
 FILE_NAME2 = 'csv_file.csv'
 
+#jsonTOcsv
+
 def reader_json():
-    with open(FILE_NAME1, 'r') as f:
-        my_dictionary = f.read()
+    with open(FILE_NAME1, 'r') as reader:
+        my_dictionary = reader.read()
     return my_dictionary
-
-def colums_for_fieldnames(my_list):
-    name_colums =[]
-    for key in my_list[0]:
-        name_colums.append(key)
-    return name_colums
     
-
 def writer_csv(my_list):
     with open(FILE_NAME2, 'w', newline='') as file:
         colums = colums_for_fieldnames(my_list)
@@ -23,5 +18,12 @@ def writer_csv(my_list):
         writer.writeheader()
         writer.writerows(my_list)
 
-my_list = json.loads(reader_json())
-writer_csv(my_list)
+def colums_for_fieldnames(my_list):
+    name_colums =[]
+    for key in my_list[0]:
+        name_colums.append(key)
+    return name_colums
+
+my_list = (reader_json())
+print(my_list)
+#writer_csv(my_list)
